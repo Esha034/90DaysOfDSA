45a536b21f13b80a7c7c2b5c9bdd8329b603c795
ESHANI BANIK <banikesha03@gmail.com>
2026-04-23 23:54:03 +0530

    Implement brute force method for max profit
    Added a brute force solution for max profit calculation.

--- Day 44/Array_44_2.py
+++ Day 44/Array_44_2.py
@@ -1,1 +1,25 @@
+class Solution:
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
 
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+
+        return max_profit
+
+
+#bruteforce
+class Solution:
+    def maxProfit(self, nums: List[int]) -> int:
+        n=len(nums)
+        max_profit=0
+        for i in range(n):
+            for j in range(i+1,n):
+                profit=0
+                if nums[i]<nums[j]:
+                    profit=nums[j]-nums[i]
+                    max_profit=max(max_profit,profit)
+        return max_profit
