6a7debc5fd5413ee281dbc6f354f43d5841c4ee0
ESHANI BANIK <banikesha03@gmail.com>
2026-05-11 01:20:54 +0530

    Implement function to count subarrays with XOR K

--- Day 54/1. Count subarrays with given xor K.py
+++ Day 54/1. Count subarrays with given xor K.py
@@ -1,1 +1,12 @@
-nn
+class Solution:
+    def subarraysWithXorK(self, nums, k):
+        count=0
+        xr=0
+        mp={0:1}
+        for num in nums:
+            xr^=num
+            x=xr^k
+            if x in mp:
+                count+=mp[x]
+            mp[xr]=mp.get(xr,0)+1
+        return count
