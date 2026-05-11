b4c5984c506f432760e961213663deb1481bdb23
ESHANI BANIK <banikesha03@gmail.com>
2026-05-11 11:27:06 +0530

    Update 2. Count Inversions.py

--- Day 54/2. Count Inversions.py
+++ Day 54/2. Count Inversions.py
@@ -1,1 +1,25 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#bruteforce
+
+class Solution:
+    def numberOfInversions(self, nums):
+        n=len(nums)
+        count=0
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]>nums[j]:
+                    count+=1
+        return count
