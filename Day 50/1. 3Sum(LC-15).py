d2b930a85c0c0549b3f7e86742af1c59b79af286
ESHANI BANIK <banikesha03@gmail.com>
2026-05-21 19:38:38 +0530

    Add brute force approach for 3Sum problem
    Implemented brute force solution for 3Sum problem.

--- Day 50/1. 3Sum(LC-15).py
+++ Day 50/1. 3Sum(LC-15).py
@@ -1,3 +1,21 @@
+#bruteforce
+
+def threeSum(nums):
+    n = len(nums)
+    res = set()
+
+    for i in range(n):
+        for j in range(i+1, n):
+            for k in range(j+1, n):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.add(tuple(sorted([nums[i], nums[j], nums[k]])))
+
+    return list(res)
+
+
+
+#optimal
+
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
 
