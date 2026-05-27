4f38c57a008d9ded48c83991459df8e73637981c
ESHANI BANIK <banikesha03@gmail.com>
2026-05-27 23:57:22 +0530

    Implement searchRange to find first and last positions

--- Day 56/1. First and last occurence(LC-34).py
+++ Day 56/1. First and last occurence(LC-34).py
@@ -1,1 +1,31 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def find_first(nums, target):
+            low, high = 0, len(nums)-1
+            first = -1
+            while low <= high:
+                mid = low + (high-low)//2
+                if nums[mid] == target:
+                    first = mid
+                    high = mid - 1  # search left
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return first
 
+        def find_last(nums, target):
+            low, high = 0, len(nums)-1
+            last = -1
+            while low <= high:
+                mid = low + (high-low)//2
+                if nums[mid] == target:
+                    last = mid
+                    low = mid + 1  # search right
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return last
+
+        return [find_first(nums, target), find_last(nums, target)]
