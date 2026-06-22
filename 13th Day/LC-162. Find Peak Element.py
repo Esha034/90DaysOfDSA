635371d4142f89740e0fb36c6855eddb6c9214d4
ESHANI BANIK <banikesha03@gmail.com>
2026-06-22 23:34:12 +0530

    Refactor findPeakElement for binary search

--- 13th Day/LC-162. Find Peak Element.py
+++ 13th Day/LC-162. Find Peak Element.py
@@ -1,15 +1,19 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        n=len(nums)
-        l,h=1,n-2
-        while l<=h:
-            mid=l+(h-l)//2
-            if nums[mid-1]<nums[mid]>nums[mid+1]:
-                return mid
-            if nums[mid-1]<nums[mid]:
-                l=mid+1
+    def findPeakElement(self, nums: list[int]) -> int:
+        n = len(nums)
+        l, h = 0, n - 1
+        
+        while l < h:
+            mid = l + (h - l) // 2
+            
+        
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
+        
             else:
-                h=mid-1
+                h = mid
+                
+        return l
         
  
 
