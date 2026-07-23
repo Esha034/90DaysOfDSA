5bf98c6ade28e7fefb85305df55bdebc94990b6c
ESHANI BANIK <banikesha03@gmail.com>
2026-07-23 23:55:01 +0530

    Update and rename Greedy to 1. #Write A Program To Find The Greatest Of Three Numbers..py

---
+++ Day 65/1. #Write A Program To Find The Greatest Of Three Numbers..py
@@ -0,0 +1,20 @@
+def find_greatest():
+    try:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+        c = int(input("Enter third number: "))
+        
+        if a>=b and a>=c:
+            print(f"{a} is the greatest number")
+        elif b>=a and b>=c:
+            print(f"{b} is the greatest number")
+        else:
+            print(f"{c} is the greatest number")
+        
+    except ValueError:
+        print("Invalid input! Please enter valid integers.")
+
+if __name__ == "__main__":
+    find_greatest()
+
+
