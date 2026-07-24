5d3dfd98f46ba866c3300ce3dd1511dba47ef432
ESHANI BANIK <banikesha03@gmail.com>
2026-07-24 08:44:09 +0530

    Update 3. Write A Program To Swap Two Numbers.py

--- Day 65/3. Write A Program To Swap Two Numbers.py
+++ Day 65/3. Write A Program To Swap Two Numbers.py
@@ -1,4 +1,4 @@
-# Write A Program To Swap Two Numbers
+#  using third variable
 
 a=int(input("Enter 1st number:"))
 b=int(input("Enter 2nd number:"))
@@ -10,6 +10,30 @@ print(f" 2nd number={b}")
 temp=a
 a=b
 b=temp
+
+print("After Swapping:")
+print(f" 1st number={a}")
+print(f" 2nd number={b}")
+
+
+
+
+
+
+
+
+
+# withoust using third variable
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+
+print("Before Swapping:")
+print(f" 1st number={a}")
+print(f" 2nd number={b}")
+
+a=a+b
+b=a-b
+a=a-b
 
 print("After Swapping:")
 print(f" 1st number={a}")
