186d41806b689263eeee14e87a82c3654fd141cc
ESHANI BANIK <banikesha03@gmail.com>
2026-08-01 22:40:57 +0530

    Create 6. Hollow Full Pyramid.py

---
+++ Day 67/6. Hollow Full Pyramid.py
@@ -0,0 +1,16 @@
+        *
+      *   *
+    *       *
+  *           *
+* * * * * * * * *
+
+#Intuition
+
+Case 1:
+First row → Print only one star
+
+Case 2:
+Middle rows → Print stars only at the boundaries
+
+Case 3:
+Last row → Print stars in every position
